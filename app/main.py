5d6953712baaa0bdb19be586d61282972d81af92
Javid-03 <javidrahman999@gmail.com>
2024-06-11 12:15:40 +0530

    test

--- app/main.py
+++ app/main.py
@@ -1,24 +1,11 @@
 from fastapi import FastAPI
 import socket
-from kubernetes import client, config
 
 app = FastAPI()
 
-# Load Kubernetes configuration from default location
-config.load_kube_config()
 
 @app.get("/")
 def test_get():
-    # Get hostname
-    host_name = socket.gethostname()
-    
-    # Get replicas of pods
-    v1 = client.CoreV1Api()
-    pod_list = v1.list_namespaced_pod(namespace="default")
-    num_replicas = len(pod_list.items)
-
-    # Prepare response headers
-    response_headers = {"X-Pod-Hostname": host_name}
-
-    # Return response
-    return {"message": "Hello EKS", "replicas": num_replicas}, 200, response_headers
+    host_name=socket.gethostname()
+    response_headers={"X-Pod-Hostname":host_name}
+    return {"message":"Hello EKS"},200,response_headers
