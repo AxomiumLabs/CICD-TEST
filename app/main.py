de786dd5256ffac17aa73f7e6628dc931d729dfa
Javid-03 <javidrahman999@gmail.com>
2024-06-11 12:44:09 +0530

    test

--- app/main.py
+++ app/main.py
@@ -6,22 +6,6 @@ app = FastAPI()
 
 @app.get("/")
 def test_get():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = ('localhost', 8000)
-    try:
-        # Connect to the server
-        sock.connect(server_address)
-
-        # Receive logs
-        while True:
-            data = sock.recv(1024)  # Adjust the buffer size as needed
-            if not data:
-                break
-            print(data.decode())  # Assuming logs are encoded in UTF-8, adjust accordingly
-
-    finally:
-        # Close the socket connection
-        sock.close()
     host_name=socket.gethostname()
     response_headers={"X-Pod-Hostname":host_name}
-    return {"message":"Hello EKS scaled","data":data},200,response_headers
+    return {"message":"Hello EKS scaled"},200,response_headers
