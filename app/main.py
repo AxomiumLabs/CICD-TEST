4ef3dade4a572683dbe115452b3b150258f91333
Javid-03 <javidrahman999@gmail.com>
2024-06-11 12:57:18 +0530

    test

--- app/main.py
+++ app/main.py
@@ -7,5 +7,6 @@ app = FastAPI()
 @app.get("/")
 def test_get():
     host_name=socket.gethostname()
+    domain=socket.getfqdn()
     response_headers={"X-Pod-Hostname":host_name}
-    return {"message":"Hello EKS scaled"},200,response_headers
+    return {"message":"Hello EKS scaled","domain":domain},200,response_headers
